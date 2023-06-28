d6cb66683f8b63b866ed7d5b7e067f6dcd9c37e7
gojenaya <gojenaya@gmail.com>
2023-06-28 17:44:29 -0400

    Created folders

--- app/index.js
+++ app/index.js
@@ -1,10 +1,17 @@
-import { View, Text } from 'react-native';
+import { useState } from 'react';
+import { View, ScrollView, SafeAreaView } from 'react-native';
+import { Stack, useRouter } from 'expo-router';
+
+import { icons, images, COLORS, SIZES } from '../constants';  
+import { Header, UserPostCard, TopNav } from '../components';
 
 const Home = () => {
+    const router = useRouter();
+    
     return (
-        <View>
-            <Text>Home</Text>
-        </View>
+        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.white }}>
+            <Stack.Screen />
+        </SafeAreaView>
     );
 };
 
